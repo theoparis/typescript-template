9d857cb1d5fad0315d1a078dbb22a4fe1521533d
Theo Paris <theoplaysmc@gmail.com>
2020-11-04 16:34:39 -0800

    Update and rename .eslintrc.json to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+    ],
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+    },
+};
