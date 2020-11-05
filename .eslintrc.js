d86ce981c9657e58db467803f6ce69290cf7a08b
Theo Paris <theoplaysmc@gmail.com>
2020-11-04 16:39:42 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,6 @@ module.exports = {
         "plugin:prettier/recommended",
     ],
     rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
     },
 };
