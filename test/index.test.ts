30d7e19d4593b62f00ed69db6d9a997939b1a478
Theo Paris <theoparisdesigns@gmail.com>
2021-07-24 21:11:25 -0700

    feat: esm BREAKING CHANGE added jest + rollup + commitlint

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { main } from "../src";
-import { expect } from "chai";
-
-describe("main()", () => {
-    it("Should contain hello", () => {
-        expect(main()).to.contain("hello");
-    });
-});
